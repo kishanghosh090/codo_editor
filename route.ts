13ded778abe237d8c0bcf273a8d813fe1ccebc79
kishanghosh090 <kishanghosh090@gamil.com>
2025-11-25 21:46:24 +0530

    add prisma models and root route.ts

---
+++ route.ts
@@ -0,0 +1,7 @@
+export const publicRoutes: string[] = [];
+
+export const protectedRoutes: string[] = [];
+
+export const authRoutes: string[] = ["/auth/sign-in",];
+
+export const apiAuthPrefix: string = "/api/auth";
