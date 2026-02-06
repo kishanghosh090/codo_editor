270ec2886c023a5dea5d858c350492fc1be4f622
kishanghosh090 <kishanghosh090@gamil.com>
2026-02-06 18:26:04 +0530

    hibernate one to many, many to one , many to many

--- auth.ts
+++ auth.ts
@@ -1,6 +1,21 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./lib/db";
+import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (!user && !account) return false;
+      const existingUser = await db.user.findUnique({
+        where: { email: user.email as string },
+      });
+      if(!existingUser){}
+    },
+    async jwt() {},
+    async session() {},
+  },
+  secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  ...authConfig,
 });
